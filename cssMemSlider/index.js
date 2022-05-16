9368f6c970d563ed0232ba28fa0e6018d947f625
Anna Makovskaya <hanna.makouskaya@newland.by>
2022-05-16 17:07:30 +0300

    feat: add images preloader (2022-05-16 17:07:30)

--- cssMemSlider/index.js
+++ cssMemSlider/index.js
@@ -33,39 +33,60 @@ const memsArray = [
     }
 ];
 
-const slider = document.querySelector('.slider');
-const memImage = document.querySelector(".mem-img img");
-const memText = document.querySelector(".mem-text");
-
-
-// add slider-buttons
-memsArray.forEach((el, index) =>{
-    let sliderItem = document.createElement("div");
-    sliderItem.classList.add("slider-element");
-    if (index === 0) sliderItem.classList.add("active");
-    sliderItem.dataset.memId = el.id;
-    sliderItem.innerHTML = "<button></button>";
-    sliderItem.addEventListener("click", function () {addMemData(el.id);});
-    slider.appendChild(sliderItem);
-});
-
-// add first mem to page
-addMemData('white');
-
-// add image and text
-function addMemData(id) {
-    memImage.classList.add('show');
-    memText.classList.add('show');
-    memImage.src = `assets/${id}.jpg`;
-    memImage.alt = `${id} mem`;
-    memText.innerHTML = memsArray.find(mem => mem.id === id).text;
-    setTimeout(function () {
-        memImage.classList.remove('show');
-        memText.classList.remove('show');
-    }, 900);
-    let activeSliderItem = document.querySelector('.slider-element.active');
-    activeSliderItem.classList.remove("active");
-    let nextActiveSliderItem = document.querySelector(`.slider-element[data-mem-id="${id}"]`);
-    nextActiveSliderItem.classList.add("active");
+// preload all mem
+let imgArray = [];
+function preload() {
+    for (let i = 0; i < arguments.length; i++) {
+        imgArray[i] = new Image();
+        imgArray[i].src = preload.arguments[i];
+    }
 }
+
+preload(
+    "assets/wine.jpg",
+    "assets/pay.jpg",
+    "assets/mama.jpg",
+    "assets/haircut.jpg",
+    "assets/goat.jpg",
+    "assets/circle.jpg",
+    "assets/8pm.jpg",
+    "assets/white.jpg"
+);
+
+
+window.onload = function() {
+    const slider = document.querySelector('.slider');
+    const memImage = document.querySelector(".mem-img img");
+    const memText = document.querySelector(".mem-text");
+
+    // add slider-buttons
+    memsArray.forEach((el, index) =>{
+        let sliderItem = document.createElement("div");
+        sliderItem.classList.add("slider-element");
+        if (index === 0) sliderItem.classList.add("active");
+        sliderItem.dataset.memId = el.id;
+        sliderItem.innerHTML = "<button></button>";
+        sliderItem.addEventListener("click", function () {addMemData(el.id);});
+        slider.appendChild(sliderItem);
+    });
+
+    // add first mem to page
+    addMemData('white');
+
+    // add image and text
+    function addMemData(id) {
+        memImage.classList.add('show');
+        memText.classList.add('show');
+        memImage.src = `assets/${id}.jpg`;
+        memImage.alt = `${id} mem`;
+        memText.innerHTML = memsArray.find(mem => mem.id === id).text;
+        setTimeout(function () {
+            memImage.classList.remove('show');
+            memText.classList.remove('show');
+        }, 900);
+        document.querySelector('.slider-element.active').classList.remove("active");
+        document.querySelector(`.slider-element[data-mem-id="${id}"]`).classList.add("active");
+    }
+};
+
 
